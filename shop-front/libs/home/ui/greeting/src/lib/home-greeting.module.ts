e94e2ef93a85ced209694c9ac5e99727cafa397c
hieugiabao <hieugiabao12@gmail.com>
2023-01-27 16:39:55 +0700

    fix(web): shop greeting ui

--- shop-front/libs/home/ui/greeting/src/lib/home-greeting.module.ts
+++ shop-front/libs/home/ui/greeting/src/lib/home-greeting.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeGreetingComponent } from './greeting.component';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule, RippleModule, RouterModule],
   declarations: [HomeGreetingComponent],
   exports: [HomeGreetingComponent],
 })
